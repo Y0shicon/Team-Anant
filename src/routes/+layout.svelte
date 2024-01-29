51fdaaf06224da6e269e57c93af67f7cb0a0993f
yoshicon <shreyaskgantayet908@gmail.com>
2024-01-30 04:20:32 +0530

    Setup : Initial setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    /** @type {import('./$types').LayoutData} */
+    export let data;
+
+    import Navbar from './Navbar.svelte';
+</script>
+
+<Navbar/>
+<slot/>
+
+<style lang="scss">
+    @import 'src/global.scss';
+</style>
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<p>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+</p>
+
+<style lang="scss">
+
+</style>
